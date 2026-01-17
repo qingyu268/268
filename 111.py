0b911c25d1ab92b2fd3fb7e865ee356de096b019
qingyu268 <3414606119@qq.com>
2026-01-17 17:03:50 +0800

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,6 @@
+from nlp_deeplearn.code.nlp_router import text_classify, sentiment_analyse, translate_text
+
+print(text_classify("股票下跌"))
+print(sentiment_analyse("我今天很开心"))
+print(translate_text("我喜欢这本书"))
+
